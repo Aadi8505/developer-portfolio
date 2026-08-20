ab944d769746fc9a3c930efc2d51e45de9cdaefc
Aaditya Kumar <111458709+Aadi8505@users.noreply.github.com>
2026-08-20 17:28:33 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -15,5 +15,5 @@ export const personalData = {
   leetcode: "https://leetcode.com/u/Aadi_8505/",
   devUsername: "Aadi8505",
   resume:
-    "https://drive.google.com/file/d/1IfAUTaUcT5BA-qyKGd49Gb23-R5Hi2VT/view?usp=sharing",
+    "https://drive.google.com/file/d/1zYleMe9SUZErxZl74HWXpAzBODG12KKb/view?usp=sharing",
 };
